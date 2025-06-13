561841ec260a0f7136f565e23a3b565e50b2ab99
okarobiono <oka.robi@gmail.com>
2025-06-13 21:31:46 +0800

    1st

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,14 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))  # sesuaikan jika folder 'docs/' ada di /project-root/docs
+
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
 html_theme = 'sphinx_rtd_theme'
 project = 'KNN Cultivation Software'
 author = 'Oka Robiono'
 release = '1.0'
 master_doc = 'index'
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+]
