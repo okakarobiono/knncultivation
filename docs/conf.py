1be69f21f5d51157fe26c7955dfe61b709321808
okarobiono <oka.robi@gmail.com>
2025-06-13 21:40:15 +0800

    1st

--- docs/conf.py
+++ docs/conf.py
@@ -2,13 +2,18 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))  # sesuaikan jika folder 'docs/' ada di /project-root/docs
 
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
+]
 html_theme = 'sphinx_rtd_theme'
 project = 'KNN Cultivation Software'
 author = 'Oka Robiono'
 release = '1.0'
 master_doc = 'index'
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-]
+
+# Tambahkan pengaturan berikut
+language = 'en'
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+html_static_path = ['_static']
